818c9a43349af1c08757302d7d194828d82ff51b
m.maselli <m.maselli@reply.it>
2020-10-04 18:03:24 +0200

    add basic auth

--- src/main/java/it/mpezzanera/ats/branch/config/SecurityConfig.java
+++ src/main/java/it/mpezzanera/ats/branch/config/SecurityConfig.java
@@ -20,7 +20,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .cors();
+                .cors()
+                .and()
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .httpBasic();
     }
 
     @Bean
